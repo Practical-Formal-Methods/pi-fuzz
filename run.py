db4a33a1c27b53229d75c5c255e2dcef92234b6d
hasanferit <eniserferit@gmail>
2021-07-05 22:55:02 +0200

    Separate hard and easy mm oracles

--- run.py
+++ run.py
@@ -22,7 +22,8 @@ def fuzz_func(fuzz_type, agent_path, bug_type, coverage):
     all_variances_mm = []
     # all_tot_warns_la = []
     # all_ind_warns_la = []
-    all_tot_warns_mm = []
+    all_tot_warns_mm_e = []
+    all_tot_warns_mm_h = []
     all_ind_warns_mm = []
     for r_id in range(N_FUZZ_RUNS):
 
@@ -49,6 +50,8 @@ def fuzz_func(fuzz_type, agent_path, bug_type, coverage):
         # plot_rq3_time(population_summaries, resulting_pools)  # plot graph
 
         warnings_mm = []
+        warnings_mm_e = []
+        warnings_mm_h = []
         for idx, fuzz_seed in enumerate(fuzzer.pool):
             game.env.set_state(fuzz_seed.state_env, fuzz_seed.data[-1])
 
@@ -62,21 +65,26 @@ def fuzz_func(fuzz_type, agent_path, bug_type, coverage):
 
             # warnings_la.append(num_warn_la)
             warnings_mm.append(num_warn_mm_tot)
+            warnings_mm_e.append(num_warn_mm_e)
+            warnings_mm_h.append(num_warn_mm_h)
 
             # logger.info("\nLookahead Oracle has found %d warnings in state %d" % (num_warn_la, idx))
             logger.info("Metamorphic Oracle has found %d(E) + %d(H) = %d warnings in state %d" % (num_warn_mm_e, num_warn_mm_h, num_warn_mm_tot, idx))
 
-        ind_warns_mm, tot_warns_mm, var_mm = post_fuzz_analysis(warnings_mm)
+        # ind_warns_mm, tot_warns_mm, var_mm = post_fuzz_analysis(warnings_mm)
+        _, tot_warns_mm_e, _ = post_fuzz_analysis(warnings_mm_e)
+        _, tot_warns_mm_h, _ = post_fuzz_analysis(warnings_mm_h)
 
         # logger.info("\nLookahead Oracle has found total of %d warnings on %.2f%% percent of states (%d). Variance is: %.2f" % (tot_warns_la, ind_warns_la, len(fuzzer.pool), var_la))
-        logger.info("\nMetamorphic Oracle has found total of %d warnings on %.2f%% percent of states (%d). Variance is: %.2f" % (tot_warns_mm, ind_warns_mm, len(fuzzer.pool), var_mm))
+        # logger.info("\nMetamorphic Oracle has found total of %d warnings on %.2f%% percent of states (%d). Variance is: %.2f" % (tot_warns_mm, ind_warns_mm, len(fuzzer.pool), var_mm))
 
         # all_variances_la.append(var_la)
         # all_ind_warns_la.append(ind_warns_la)
         # all_tot_warns_la.append(tot_warns_la)
-        all_variances_mm.append(var_mm)
-        all_ind_warns_mm.append(ind_warns_mm)
-        all_tot_warns_mm.append(tot_warns_mm)
+        # all_variances_mm.append(var_mm)
+        # all_ind_warns_mm.append(ind_warns_mm)
+        all_tot_warns_mm_e.append(tot_warns_mm_e)
+        all_tot_warns_mm_h.append(tot_warns_mm_h)
 
         fuzz_et = time.time()
 
@@ -91,11 +99,11 @@ def fuzz_func(fuzz_type, agent_path, bug_type, coverage):
     # logger.info("    Variance in number of warnings found in states: %s" % str(all_variances_la))
     # logger.info("    Percentage of states with at least one warning: %s" % str(all_ind_warns_la))
     logger.info("Metamorphic Oracle summary in %d fuzz runs:" % N_FUZZ_RUNS)
-    logger.info("    Total number of warnings: %s" % str(all_tot_warns_mm))
+    # logger.info("    Total number of warnings: %s" % str(all_tot_warns_mm))
     logger.info("    Variance in number of warnings found in states: %s" % str(all_variances_mm))
     logger.info("    Percentage of states with at least one warning: %s" % str(all_ind_warns_mm))
 
-    return all_tot_warns_mm, all_ind_warns_mm, all_variances_mm  # all_tot_warns_la, all_ind_warns_la, all_variances_la,
+    return all_tot_warns_mm_e, all_tot_warns_mm_h, all_ind_warns_mm, all_variances_mm  # all_tot_warns_la, all_ind_warns_la, all_variances_la,
 
 
 fuzz_start_time = time.strftime("%Y%m%d_%H%M%S")
@@ -135,9 +143,9 @@ for idx, pp in enumerate(ppaths):
     logger.info("**************************************")
     logger.info("Random Action Mutator and Queue Scheduler will be used.")
 
-    tot_mm, ind_mm, var_mm = fuzz_func(fuzz_type, pp, bug_type, coverage)
+    tot_mm_e, tot_mm_h, ind_mm, var_mm = fuzz_func(fuzz_type, pp, bug_type, coverage)
 
-    report = [bug_type, coverage, pname, str(RANDOM_SEEDS), str(tot_mm), str(ind_mm), str(var_mm)]
+    report = [bug_type, coverage, pname, str(RANDOM_SEEDS), str(tot_mm_e), str(tot_mm_h), str(ind_mm), str(var_mm)]
     worksheet.write_row(idx, 0, report)
 
     # with open("results/outs.csv", mode="a") as fw:
