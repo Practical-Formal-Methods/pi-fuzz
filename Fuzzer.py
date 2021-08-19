f05f1b33f21f8d16de12717b7c4c2903c00bfbcc
hasanferit <eniserferit@gmail>
2021-08-19 12:54:01 +0200

    Update fuzzing scheme

--- Fuzzer.py
+++ Fuzzer.py
@@ -11,13 +11,14 @@ from fuzz_config import FUZZ_BUDGET
 logger = logging.getLogger('fuzz_logger')
 
 class Fuzzer:
-    def __init__(self, r_seed, fuzz_type, fuzz_game, coverage, coverage_thold, mut_budget):
+    def __init__(self, r_seed, fuzz_type, fuzz_game, use_seedp, coverage, coverage_thold, mut_budget):
 
         self.rng = np.random.default_rng(r_seed)
         self.fuzz_type = fuzz_type
         self.game = fuzz_game
         self.cov_type = coverage
         self.cov_thold = coverage_thold
+        self.use_seedp = use_seedp
 
         self.pool = []
         self.epochs = 0
@@ -25,7 +26,7 @@ class Fuzzer:
 
         self.random_action_mutator = Mutator.RandomActionMutator(self.game, mut_budget)
         self.seed_policy_mutator = Mutator.SeedPolicyMutator(self.game, mut_budget)
-        self.schedule = Scheduler.QueueScheduler()
+        self.schedule = Scheduler.RandomScheduler()
 
     # @profile
     def fuzz(self):
@@ -41,21 +42,34 @@ class Fuzzer:
             trial += 1
             rnd = self.rng.random()
 
-            if rnd < 0.8:
-                if self.fuzz_type == "gbox":
-                    seed = self.schedule.choose(self.pool)
-                else:  # bbox
-                    self.game.env.reset()  # rng=self.rng)
-                    cand_nn, cand_hi_lvl = self.game.get_state()
-                    seed = Seed(cand_nn, cand_hi_lvl, trial, time.perf_counter()-start_time)
-
-                if rnd < 0.4:
-                    cand_nn, cand_hi_lvl = self.seed_policy_mutator.mutate(seed, self.rng)
-                else:  # iow ->  rnd >= 0.4 and rnd < 0.8:
-                    cand_nn, cand_hi_lvl = self.random_action_mutator.mutate(seed, self.rng)
+            if self.fuzz_type == "gbox" and rnd < 0.7:
+                seed = self.schedule.choose(self.pool, self.rng)
             else:
                 self.game.env.reset()  # rng=self.rng)
                 cand_nn, cand_hi_lvl = self.game.get_state()
+                seed = Seed(cand_nn, cand_hi_lvl, trial, time.perf_counter()-start_time)
+
+            rnd = self.rng.random()
+            if self.use_seedp and rnd < 0.5:
+                cand_nn, cand_hi_lvl = self.seed_policy_mutator.mutate(seed, self.rng)
+            else:
+                cand_nn, cand_hi_lvl = self.random_action_mutator.mutate(seed, self.rng)
+
+            # if rnd < 0.8:
+            #     if self.fuzz_type == "gbox":
+            #         seed = self.schedule.choose(self.pool, self.rng)
+            #     else:  # bbox
+            #         self.game.env.reset()  # rng=self.rng)
+            #         cand_nn, cand_hi_lvl = self.game.get_state()
+            #         seed = Seed(cand_nn, cand_hi_lvl, trial, time.perf_counter()-start_time)
+            #
+            #     if rnd < 0.4:
+            #         cand_nn, cand_hi_lvl = self.seed_policy_mutator.mutate(seed, self.rng)
+            #     else:  # iow ->  rnd >= 0.4 and rnd < 0.8:
+            #         cand_nn, cand_hi_lvl = self.random_action_mutator.mutate(seed, self.rng)
+            # else:
+            #     self.game.env.reset()  # rng=self.rng)
+            #     cand_nn, cand_hi_lvl = self.game.get_state()
 
             if cand_nn is not None and self.is_interesting(cand_nn):
                 cur_time = time.perf_counter()-start_time
