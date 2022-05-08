ec3c34b7d65fd4bcffe3890bf49a7990cbe0c227
hfeniser <eniserferit@gmail.com>
2022-05-08 14:59:56 +0200

    Remove submodule

---
+++ mod_stable_baselines3/stable_baselines3/a2c/__init__.py
@@ -0,0 +1,2 @@
+from mod_stable_baselines3.stable_baselines3.a2c.a2c import A2C
+from mod_stable_baselines3.stable_baselines3.a2c.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
---
+++ mod_stable_baselines3/stable_baselines3/ddpg/__init__.py
@@ -0,0 +1,2 @@
+from mod_stable_baselines3.stable_baselines3.ddpg.ddpg import DDPG
+from mod_stable_baselines3.stable_baselines3.ddpg.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
